2386a36e7103fea79b600b55c064e81b5e52c9eb
Rumy Hasan <Rumy-hasan@users.noreply.github.com>
2022-07-22 13:37:56 +0600

    Initial Commit

---
+++ AttendanceSystem/AttendanceSystemApp.swift
@@ -0,0 +1,17 @@
+//
+//  AttendanceSystemApp.swift
+//  AttendanceSystem
+//
+//  Created by Paradox Space Rumy M1 on 22/7/22.
+//
+
+import SwiftUI
+
+@main
+struct AttendanceSystemApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
